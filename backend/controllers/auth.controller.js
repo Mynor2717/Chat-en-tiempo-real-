bbaa00be361fd4487e1b1f74962242eae3abe65a
node <ratafak15@gmail.com>
2024-08-14 05:24:01 -0400

    Manejo de las rutas del servidor y sus funciones

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const signup = (req, res) => {
+    res.send("SignupUser");
+    console.log("signupUser");
+}
+
+export const login = (req, res) => {
+    console.log("loginUser");
+}
+
+export const logout = (req, res) => {
+    console.log("logoutUser");
+}
+
